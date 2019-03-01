4e4a3a4cc8bc902406a90b3dd3785cecfbc5c9dc
Emraji <emmanu-0000@hotmail.com>
2019-03-01 12:15:56 -0500

    Definiendo esqueleto de contenido

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Manu</title>
+  </head>
+  <body>
+    <!-- encabezado -->
+    <!-- promotores -->
+    <!-- contenido1 -->
+    <!-- contenido2 -->
+    <!-- recomendaciones1 -->
+    <!-- banner de trabajo -->
+    <!-- recomendaciones2 -->
+    <!-- soporte -->
+    <!-- link de invitacion -->
+    <!-- precios -->
+    <!-- preguntas frecuentes -->
+    <!-- formulario de subscripción -->
+    <!-- pie de pagina -->
+  </body>
+</html>
