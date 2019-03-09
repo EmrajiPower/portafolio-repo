1ae5d4522a109a17cfc7f524319faebf8aaa89c5
Emraji <32020190+EmrajiPower@users.noreply.github.com>
2019-03-08 19:46:35 -0500

    Agregando encabezado (1440px)

--- index.php
+++ index.php
@@ -3,13 +3,39 @@
   <head>
     <meta charset="utf-8">
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="index.css">
     <title>Manu</title>
   </head>
   <body>
     <!-- encabezado -->
+    <header class="info-container">
+      <img src="" alt="" width="60" height="60">
+      <div class="lista-container">
+        <ol>
+          <li>
+            <a href="#">Precios</a>
+          </li>
+          <li>
+            <a href="#">Soporte</a>
+          </li>
+          <li>
+            <a href="#">Preguntas<br>Frecuentes</a>
+          </li>
+        </ol>
+      </div>
+    </header>
     <!-- promotores -->
+    <section class="info-container">
+
+    </section>
     <!-- contenido1 -->
+    <section class="info-container">
+
+    </section>
     <!-- contenido2 -->
+    <section class="info-container">
+
+    </section>
     <!-- recomendaciones1 -->
     <!-- banner de trabajo -->
     <!-- recomendaciones2 -->
