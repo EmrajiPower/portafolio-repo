f114c63f1154663d1bb196349745e0f2de618598
Emmanuel <emmanu-0000@hotmail.com>
2019-03-10 03:52:17 -0500

    Agregando formulario de Soporte y Conexión php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
       <div class="lista-container">
         <ol>
           <li>
-            <a href="#">Precios</a>
+            <a href="#">Proyectos</a>
           </li>
           <li>
             <a href="#">Soporte</a>
@@ -40,6 +40,48 @@
     <!-- banner de trabajo -->
     <!-- recomendaciones2 -->
     <!-- soporte -->
+    <section class="formulario-container">
+      <form class="formulario-soporte" action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+      <div class="columna-1">
+        <label for="nombre-persona">Nombre</label>
+        <input type="text" name="nombre-persona" required placeholder="Tu nombre">
+        <br>
+        <label for="nombre-empresa">Nombre de tu empresa</label>
+        <input type="text" name="nombre-empresa" required placeholder="Tu empresa">
+        <br>
+        <label for="nombre-queja">Tipo de queja</label>
+        <select name="nombre-queja">
+          <option value="" selected disabled hidden>Seleccione una</option>
+          <option value="reclamo">Reclamos</option>
+          <option value="fallo">Fallos</option>
+          <option value="mantenimiento">Mantenimiento</option>
+        </select>
+      </div>
+      <div class="columna-2">
+        <label for="comentarios">Comentarios</label>
+        <br>
+        <textarea name="comentarios" rows="10" cols="80"></textarea>
+        <input type="submit" name="btn-soporte" value="Enviar">
+      </div>
+        <?php
+          if (isset($_POST['btn-soporte'])) {
+            include("informacion.php");
+            $np = $_POST['nombre-persona'];
+            $ne = $_POST['nombre-empresa'];
+            $tq = $_POST['nombre-queja'];
+            $com = $_POST['comentarios'];
+
+            $sql = "INSERT INTO soporte (nombre_persona,nombre_empresa,tipo_queja,comentario)
+            VALUES ('$np','$ne','$tq','$com')";
+
+            $conexion->query($sql);
+            echo "<p>Se guardó la Información 🌟</p>";
+            // Cerrar Conexión
+            // mysql_close($conexion);
+          }
+         ?>
+      </form>
+    </section>
     <!-- link de invitacion -->
     <!-- precios -->
     <!-- preguntas frecuentes -->
--- informacion.php
+++ informacion.php
@@ -1,3 +1,17 @@
 <?php
-  
- ?>
+$server = "localhost";
+$usuario = "root";
+$contrasena = "";
+$bd = "portafolio";
+
+// Tablas
+$nombre_tabla = "soporte";
+
+// Conexión
+$conexion = new mysqli($server,$usuario,$contrasena,$bd);
+
+if ($conexion->connect_error) {
+  die("<p>Error, Sin conexión a la base de Datos ".$nombre_tabla);
+}
+
+?>
